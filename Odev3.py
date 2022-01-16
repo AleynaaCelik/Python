10d8b6a91562f504b0423bd0ad8c2eee1471f988
Aleyna ÇELİK <77541289+Aleyna06@users.noreply.github.com>
2022-01-16 09:41:55 +0300

    Add files via upload

---
+++ Odev3.py
@@ -0,0 +1,21 @@
+sinavnotu=int(input('Notunuzu giriiniz'))
+if sinavnotu>100:
+    print("NOTUNUZ GEÇERSİZ")
+if 90<sinavnotu<100:
+    print("Notunuz AA") 
+if 85<sinavnotu<89:
+    print("Notunuz BA")
+if 80<sinavnotu<84:
+    print("Notunuz BB")
+if 75<sinavnotu<79:
+    print("Notunuz CB")
+if 70<sinavnotu<74:
+    print("Notunuz CC")
+if 65<sinavnotu<69:
+    print("Notunuz BA")
+if 60<sinavnotu<64:
+    print("Notunuz DD")
+if 50<sinavnotu<59:
+    print("Notunuz FD")
+if 0<sinavnotu<49:
+    print("Notunuz FF")
